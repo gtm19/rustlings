5410ee954d8513b19325b6722cf1459760299b79
Gareth Maddock <gtmaddock@gmail.com>
2025-11-27 21:37:00 -0400

    more rustlings

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,4 +1,8 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
+fn call_me() {
+  let _message = "Hello from call_me function!";
+  println!("{}", _message);
+}
 
 fn main() {
     call_me(); // Don't change this line
